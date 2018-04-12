0bfe8e8127e3dc6a54b7af23fb4433e73b3911ed
Micha Livne <mlivne@cs.toronto.edu>
2018-04-12 08:39:29 -0400

    1. Fixed beta1 of Adam optimizer.

--- src/examples/dcgan.py
+++ src/examples/dcgan.py
@@ -30,7 +30,7 @@ parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
-parser.add_argument('--beta1', type=float, default=0.9, help='beta1 for adam. default=0.9')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
@@ -255,10 +255,7 @@ try:
 
             output = netD(inputv)
             errD_real = criterion(output, labelv)
-            if epoch > 0:
-                errD_real = torch.clamp(freq_filter.step({"train.errD_real": errD_real})["train.errD_real"], 1e-6)
-            else:
-                freq_filter.step({"train.errD_real": errD_real})
+            errD_real = torch.clamp(freq_filter.step({"train.errD_real": errD_real})["train.errD_real"], 1e-6)
             errD_real.backward()
             D_x = output.data.mean()
 
@@ -269,10 +266,7 @@ try:
             labelv = Variable(label.fill_(fake_label))
             output = netD(fake.detach())
             errD_fake = criterion(output, labelv)
-            if epoch > 0:
-                errD_fake = torch.clamp(freq_filter.step({"train.errD_fake": errD_fake})["train.errD_fake"], 1e-6)
-            else:
-                freq_filter.step({"train.errD_fake": errD_fake})
+            errD_fake = torch.clamp(freq_filter.step({"train.errD_fake": errD_fake})["train.errD_fake"], 1e-6)
             errD_fake.backward()
             D_G_z1 = output.data.mean()
 
@@ -289,10 +283,7 @@ try:
             labelv = Variable(label.fill_(real_label))  # fake labels are real for generator cost
             output = netD(fake)
             errG = criterion(output, labelv)
-            if epoch > 0:
-                errG = torch.clamp(freq_filter.step({"train.errG": errG})["train.errG"], 1e-6)
-            else:
-                freq_filter.step({"train.errG": errG})
+            errG = torch.clamp(freq_filter.step({"train.errG": errG})["train.errG"], 1e-6)
             errG.backward()
             D_G_z2 = output.data.mean()
 
